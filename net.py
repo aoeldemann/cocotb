12621ff04bce4fa877dd0fc611521eb6f832fd0d
Andreas Oeldemann <andreas.oeldemann@tum.de>
2017-08-30 21:48:58 +0200

    net: added toeplitz hash calculation

--- net.py
+++ net.py
@@ -2,7 +2,7 @@
 # Project:        cocotb
 # File:           net.py
 # Date Create:    May 17th 2017
-# Date Modified:  May 17th 2017
+# Date Modified:  August 30th 2017
 # Author:         Andreas Oeldemann, TUM <andreas.oeldemann@tum.de>
 #
 # Description:
@@ -14,6 +14,7 @@ from scapy.all import *
 from random import randint
 from math import log
 from array import array
+from netaddr import IPAddress
 
 def gen_packet():
     """Generate a random IP v4/v6, TCP/UDP packet.
@@ -79,3 +80,62 @@ def axis_data_to_packet(tdata, tkeep, datapath_bit_width):
             pkt_data.append(tdata_word & 0xFF)
             tdata_word >>= 8
     return Ether(pkt_data.tostring())
+
+def calc_toeplitz_hash(pkt, key, key_len):
+    """Calculates the Toeplitz hash value for an IPv4/IPv4 + TCP/UDP packet.
+
+
+    The function calculates the Toeplitz hash value for an IPv4/IPv4 + TCP/UDP
+    packet. This value is commonly used for receive side scaling. The function
+    expects three inputs: 1) a scapy packet, 2) the hash function key and
+    3) the length of the hash function key in bytes.
+
+    In its current implementation, the hash value is only calculated for
+    IPv4/IPv6 packets that contain a TCP or UDP payload. For non-IP packets an
+    error is thrown. For IP packets that do not contain a TCP or UDP payload,
+    a hash value of zero is returned.
+    """
+
+    # only calculate toeplitz hash for IPv4 and IPv6 packets
+    assert (IP in pkt) or (IPv6 in pkt)
+
+    if IP in pkt: # L3 is IPv4
+        # L3 is IPv4
+        data = int(IPAddress(pkt[IP].src)) << 64
+        data |= int(IPAddress(pkt[IP].dst)) << 32
+        l3 = IP
+    elif IPv6 in pkt: # L3 is IPv6
+        data = int(IPAddress(pkt[IPv6].src, 6)) << 160
+        data |= int(IPAddress(pkt[IPv6].dst, 6)) << 32
+        l3 = IPv6
+
+
+    if TCP in pkt[l3]: # L4 is TCP
+        data |= pkt[l3][TCP].sport << 16
+        data |= pkt[l3][TCP].dport
+    elif UDP in pkt[l3]: # L4 is UDP
+        data |= pkt[l3][UDP].sport << 16
+        data |= pkt[l3][UDP].dport
+    else:
+        # L4 is neither TCP, nor UDP. for now we return a hash value of zero
+        return 0
+
+    # determine overall length of data to be hashed
+    if l3 == IP:
+        dataLen = 96
+    elif l3 == IPv6:
+        dataLen = 288
+
+    # initialize data mask
+    dataMask = 1 << (dataLen - 1)
+
+    # initialize hash value
+    hashval = 0
+
+    # do the hashing
+    for i in range(dataLen):
+        if data & dataMask:
+           hashval ^= (key >> (key_len * 8 - 32 - i)) & 0xFFFFFFFF
+        dataMask = dataMask >> 1
+
+    return hashval
